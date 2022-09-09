37c09b2a8db0a72d8df8b784feaa87d6e2a575a8
Alex Burnett <ab039920@ohio.edu>
2022-09-09 14:12:41 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Alex Burnett>
+ *        Date: <9/9/2022>
+ * Description: Description (Added)
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
